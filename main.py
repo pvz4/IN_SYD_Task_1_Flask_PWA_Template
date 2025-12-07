2516f1f912677315719ee730f4588a5943a21aad
pvz4 <ryan.algarve@education.nsw.gov.au>
2025-12-07 14:06:23 +0000

    commit

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ import database_manager as dbHandler
 app = Flask(__name__)
 
 @app.route('/index.html', methods=['GET'])
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
    data = dbHandler.listExtension()
    return render_template('/index.html', content=data)
-
+@app.route('/wip.html', methods=['GET'])
+def wip():
+   return render_template('/wip.html')
+@app.route('/about.html', methods=['GET'])
+def about():
+   return render_template('/about.html')
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
